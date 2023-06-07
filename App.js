bdb3e4b0c654a41e338ebaa48d09bf6d4c6afbfc
Felipe Rotermel <feliperotermell@gmail.com>
2023-06-07 12:03:38 -0300

    App em ivo1

--- App.js
+++ App.js
@@ -6,16 +6,7 @@ import Routes from './src/routes.js';
 export default function App() {
   return (
     <Fragment>
-    <View style={styles.container}>
-      <Text style={styles.seta} >⇓⇓⇓</Text>
-      <Image style={styles.ivo} source={{uri:'https://sig.ifc.edu.br/shared/verFoto?idFoto=515190&key=1a2605f7d244fbddc36dac08cf529620'}}></Image>
       <Routes />
-      <Text style={styles.textu}>Sou gay!</Text>
-      <Text style={styles.textu}>Ivo gaymer</Text>
-      <Text style={styles.odio}>Eu te odeio!</Text>
-      <Text style={styles.assinado}>ass: ivo gaymer da silva</Text>
-      <StatusBar style="dark-content" />
-    </View>
     </Fragment>
   );
 }
--- src/pages/Perfil/index.js
+++ src/pages/Perfil/index.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function Perfil() {
     return (
         <View style={styles.container}> 
-            <Image style={styles.ivo} source={{uri:'https://sig.ifc.edu.br/shared/verFoto?idFoto=515190&key=1a2605f7d244fbddc36dac08cf529620'}}></Image>
+              <View style={styles.container}>
+      <Text style={styles.seta} >⇓⇓⇓</Text>
+      <Image style={styles.ivo} source={{uri:'https://sig.ifc.edu.br/shared/verFoto?idFoto=515190&key=1a2605f7d244fbddc36dac08cf529620'}}></Image>
+      <Text style={styles.textu}>Sou gay!</Text>
+      <Text style={styles.textu}>Ivo gaymer</Text>
+      <Text style={styles.odio}>Eu te odeio!</Text>
+      <Text style={styles.assinado}>ass: ivo gaymer da silva</Text>
+    </View>
         </View>
     );
 }
