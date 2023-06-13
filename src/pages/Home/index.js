0d13b8cccd33f68aec3efe7f19e0eb84dbdb6c25
Felipe Rotermel <feliperotermell@gmail.com>
2023-06-13 16:41:06 -0300

    Perfil and Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,84 +2,36 @@ import React from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
 
 export default function Home() {
-    return (
-        <View style={styles.container}> 
-            <View style={styles.tudo}>
-                <View style={styles.menos}>
-                    <Text style={styles.textu}>"Sou gay!"</Text>
-                    <Text style={styles.textu}>-Ivo gaymer 2023</Text>
-                </View>
-                <View style={styles.barbada}>
-                <Text style={styles.seta} >⇓⇓⇓</Text>
-                <Image style={styles.ivo} source={{uri:'https://sig.ifc.edu.br/shared/verFoto?idFoto=515190&key=1a2605f7d244fbddc36dac08cf529620'}}></Image>
-                </View>
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.container}> 
+      <Text style={styles.text}>Irato</Text>
+      <Image style={styles.img} source={{uri:'https://cdn.discordapp.com/attachments/1061039263296262285/1118262468565794978/ecdb3c19-1062-4bc4-8aa5-ab2ad78c65c1.jpg'}}></Image>  
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'row'
-
-    },
-    tudo:{
-        flexDirection: 'row',
-    },
-    menos:{
-        textAlign: 'center',
-        justifyContent: 'center',
-    },
-    barbada:{
-      alignItems: 'center',
-    },
-    container: {
-        flex: 1,
-        backgroundColor: '#252525',
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: '#fff',
-      },
-      textu: {
-        color: '#fff',
-        flexDirection:'column',
-        textDecorationStyle: 'solid',
-        fontSize: 40,
-        textDecorationColor: '#fff',
-        textShadowOffset: {width: 5, height: 5},
-        textShadowRadius: 10,
-        textShadowColor: '#fff',
-      },
-      ivo:{
-        width: 200,
-        height: 200,
-        resizeMode: 'stretch',
-      },
-      odio:{
-        color: 'red',
-        textDecorationStyle: 'solid',
-        fontSize: 70,
-        textDecorationLine: 'underline',
-        textDecorationColor: 'crimson',
-        textShadowOffset: {width: 5, height: 5},
-        textShadowRadius: 10,
-        textShadowColor: 'black',
-      },
-      assinado:{
-        color: 'grey',
-        textDecorationStyle: 'solid',
-        fontSize: 10,
-        textDecorationLine: 'underline',
-        textDecorationColor: 'black',
-        textShadowOffset: {width: 5, height: 5},
-        textShadowRadius: 10,
-        textShadowColor: 'grey',
-      },
-      seta:{
-        fontSize: 50,
-      }
+  container: {
+    flex: 1,
+    backgroundColor: '#252525',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#fff',
+  },
+  text: {
+    color: '#fff',
+    flexDirection:'column',
+    textDecorationStyle: 'solid',
+    fontSize: 40,
+    textDecorationColor: '#fff',
+    textShadowOffset: {width: 5, height: 5},
+    textShadowRadius: 10,
+    textShadowColor: '#fff',
+  },
+  img:{
+    width: 200,
+    height: 200,
+    resizeMode: 'stretch',
+    borderRadius: 100,
+  },
 });
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,16 @@
 import react from "react";
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MaterialIcons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Home from "./pages/Home";
 import Busca from "./pages/Busca";
 import Perfil from "./pages/Perfil";
 import Pedidos from "./pages/Pedidos";
 import Carteira from "./pages/Carteira";
+import Pagamentos from "./pages/Pagamentos";
 
 const BottomTab = createBottomTabNavigator();
 
@@ -38,14 +39,30 @@ export default function Routes() {
                 ),
             }} 
             />
-            <BottomTab.Screen name="Perfil" component={Perfil} options={{
-                tabBarLabel: 'Perfil',
-                tabBarIcon: ({ color }) => (
-                    <MaterialIcons name="person" color={color} size={26} />
-                ),
-            }} 
-            />         
+            <BottomTab.Screen
+          name="PerfilRoutes"
+          component={PerfilRoutes}
+          options={{
+            headerShown: false,
+            tabBarLabel: 'Perfil',
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="person" color={color} size={26} />
+            ),
+          }}
+        />         
         </BottomTab.Navigator>    
     </NavigationContainer>
   );
+}
+
+export function PerfilRoutes() {
+    const Stack = createNativeStackNavigator();
+    
+    return (
+        <Stack.Navigator>
+        <Stack.Screen name="Perfil" component={Perfil} />
+        <Stack.Screen name="Carteira" component={Carteira} />
+        <Stack.Screen name="Pagamentos" component={Pagamentos} />
+        </Stack.Navigator>
+    );
 }
